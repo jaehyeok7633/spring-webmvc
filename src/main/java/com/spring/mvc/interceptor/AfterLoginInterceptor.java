5a65703a83d1a64fbf278a772359594428218f8f
Jaehyeok <jaehyeok7633@naver.com>
2024-04-04 13:24:57 +0900

    #28. 게시판 인가처리 인터셉터 설정

--- src/main/java/com/spring/mvc/interceptor/AfterLoginInterceptor.java
+++ src/main/java/com/spring/mvc/interceptor/AfterLoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.spring.mvc.interceptor;
 
+import com.spring.mvc.util.LoginUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -7,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.io.PrintWriter;
+
+import static com.spring.mvc.util.LoginUtils.*;
 
 @Configuration
 public class AfterLoginInterceptor implements HandlerInterceptor {
@@ -16,13 +19,12 @@ public class AfterLoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-
         // 세션 받아오기
         HttpSession session = request.getSession();
 
         // 세션 데이터 중 login이라는 이름의 데이터가 있는 지 확인한 후에
         // 존재한다면 요청이 컨트롤러로 들어가지 못하게 하겠다.
-        if (session.getAttribute("login") != null) {
+        if (isLogin(session)) {
 
             response.setContentType("text/html; charset=UTF-8");
             PrintWriter w = response.getWriter();
@@ -45,11 +47,25 @@ public class AfterLoginInterceptor implements HandlerInterceptor {
             w.write(htmlCode);
             w.flush();
 
-            return false; //컨트롤러로 들어가는 요청을 막음.
+            return false; // 컨트롤러로 들어가는 요청을 막음.
         }
 
         return true; // 로그인 안했으면 통과~
 
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
